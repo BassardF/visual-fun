6060f5262687e67db0df13f9b20f3df4caa4de95
Frank <fbassard@wayup.com>
2019-05-18 20:30:05 -0400

    Add TF

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ReactSpringPage from './components/react-spring/Page';
+import TensorFlow from './components/tensor-flow/TensorFlow';
 
 
 export default function AppRouter() {
     return (
         <Router>
             <Route path="/" exact component={ReactSpringPage} />
+            <Route path="/tf" exact component={TensorFlow} />
         </Router>
     );
   }
